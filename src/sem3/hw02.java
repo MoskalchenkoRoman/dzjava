5fa717f12ace00f7aaa31437b754a7bbb7f7a0b4
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2022-12-15 13:59:41 +0300

    dz3.1

--- src/sem3/hw02.java
+++ src/sem3/hw02.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class hw02 {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(2,5,7,9,5,6,7,3,8,7,25));   // чтобы использовать add удалить из скобок !
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(2,142,180,53,122,5,7,9,5,6,44,32,7,3,8,7,25,456,589,14,58,66,54,9));   // чтобы использовать add удалить из скобок !
 //        array.add(45);
 //        array.add(18);
 //        array.add(3);
@@ -18,12 +18,14 @@ public class hw02 {
         System.out.println(array);
 
         for (int i = 0; i < array.size(); i++) {
-            if (array.get(i)%2==0) {
+            int x = array.get(i);
+            if ((x%2) == 0) {
                 array.remove(i);
+                i--;
             }
         }
-
         System.out.println(array);
+
     }
 
 }
